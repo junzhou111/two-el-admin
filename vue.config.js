cdf92c84375850ce9a2010d708a8acee1b46b44c
junzhou <3306299233@qq.com>
2022-09-29 10:04:18 +0800

    feat(custom): 配置基础配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 解决打包白屏问题
+  devServer:{
+    prop:8888,//端口号
+    host:"localhost",//
+    open:true,
+    https:false,//是否开启加密协议
+
+  },
+  lintOnSave:false,//关闭严格模式
+  productionSourceMap:false,
 })
